3291197ce3fc0ef8029386977cfd742f67e1457a
zaid638 <142732644+zaid638@users.noreply.github.com>
2024-06-30 22:30:32 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
-from temp_db import read_db, save_db, package
+import datetime
 
 app = Flask(__name__)
 
@@ -12,7 +12,6 @@ app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'Zaid'
 app.config['MYSQL_PASSWORD'] = '22155'
 app.config['MYSQL_DB'] = 'tourism'
-
 
 mysql = MySQL(app)
 
@@ -26,7 +25,6 @@ def packages():
     cursor1 = mysql.connection.cursor()
     cursor1.execute("SELECT * FROM packages")
     result1 = cursor1.fetchall()
-
     pkges = []
 
     for i in result1:
@@ -34,7 +32,7 @@ def packages():
         sub.append(i[0])
         sub.append(i[1])
         cursor2 = mysql.connection.cursor()
-        cursor2.execute(f"SELECT location_name FROM locations where package_id={i[0]}")
+        cursor2.execute(f"SELECT location_name FROM view_packages2 where package_id={i[0]}")
         result2 = cursor2.fetchall()
         loc = []
         for j in result2:
@@ -69,14 +67,12 @@ def login():
 
 
 
-
 @app.route('/userpackages')
 def userpackages():
 
     cursor1 = mysql.connection.cursor()
     cursor1.execute("SELECT * FROM packages")
     result1 = cursor1.fetchall()
-
     pkges = []
 
     for i in result1:
@@ -84,7 +80,7 @@ def userpackages():
         sub.append(i[0])
         sub.append(i[1])
         cursor2 = mysql.connection.cursor()
-        cursor2.execute(f"SELECT location_name FROM locations where package_id={i[0]}")
+        cursor2.execute(f"SELECT location_name FROM view_packages2 where package_id={i[0]}")
         result2 = cursor2.fetchall()
         loc = []
         for j in result2:
@@ -93,16 +89,15 @@ def userpackages():
         sub.append(i[2])
         sub.append(i[3])
         pkges.append(sub)        
-    return render_template("packages2.html", pkge=pkges)
+    return render_template("userpackages.html", pkge=pkges)
 
 
 @app.route('/')
 def logout():
     session.pop('loggedin', None)
     session.pop('username', None)
+    session.pop('id', None)
     return render_template('index.html')
-
-
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -138,16 +133,203 @@ def register():
     
 
 
-@app.route("/history")
+@app.route("/history", methods=['GET', 'POST'])
 def history():
-
     cursor = mysql.connection.cursor()
-    cursor.execute('SELECT booking_id, package_id, booking_date, number_of_tickets, status FROM bookings  WHERE user_id = % s', (session['id'], ))
-    result = cursor.fetchall()
-       
+    cursor.execute('SELECT booking_id, package_id, booking_date, number_of_tickets, status FROM bookings  WHERE user_id = % s ORDER BY booking_id DESC', (session['id'], ))
+    result = cursor.fetchall()       
     return render_template("booking_history.html", data=result)
 
 
+@app.route('/booking', methods=['GET', 'POST'])
+def booking():
+
+    if request.method == 'POST' and 'PackageID' in request.form and 'Tickets' in request.form:
+        PackageID = request.form['PackageID']
+        Ticket = request.form['Tickets']
+        cursor2 = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+
+        if not re.match(r'[0-9]+', Ticket):
+            msg = 'name must contain only numbers !'
+        else:
+            cursor2.execute('INSERT INTO bookings VALUES (NULL, % s, % s, % s, % s, % s)',
+                           (PackageID, session['id'], datetime.datetime.now().strftime("%Y-%m-%d"), Ticket, "pending", ))
+            mysql.connection.commit()
+            msg = 'You have successfully booked we will confirm your bookng in 24 hrs!'
+            return redirect(url_for('history'))
+        
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+
+    pkgID = request.form['pkgID']
+    pkgName = request.form['pkgName']
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT location_name, discription FROM locations, package_details WHERE locations.location_id = package_details.location_id AND package_details.package_id = % s', (pkgID, ))
+    result = cursor.fetchall()
+
+    return render_template('booking_form.html', data = result, data2 = pkgName, data3 = pkgID)
+
+
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    cursor1 = mysql.connection.cursor()
+    cursor2 = mysql.connection.cursor() 
+    cursor3 = mysql.connection.cursor()  
+    cursor4 = mysql.connection.cursor()
+    cursor1.execute('SELECT count(*) FROM users')
+    cursor2.execute('SELECT count(*) FROM packages')
+    cursor3.execute('SELECT count(*) FROM bookings')
+    cursor4.execute('SELECT count(*) FROM bookings where status="pending"')
+    USERS = cursor1.fetchone()    
+    PACKAGES = cursor2.fetchone()        
+    BOOKINGS = cursor3.fetchone()        
+    PENDING = cursor4.fetchone()        
+    return render_template('admin_home.html', users=USERS, packages=PACKAGES, bookings=BOOKINGS, pending=PENDING)
+
+
+
+@app.route("/manage_users", methods=['GET', 'POST'])
+def manage_users():
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM users')
+    result = cursor.fetchall()       
+    return render_template("manage_users.html", data=result)
+
+
+@app.route("/manage_bookings", methods=['GET', 'POST'])
+def manage_bookings():
+
+    if request.method == 'POST' and 'bkID' in request.form:
+        bookingID = request.form['bkID']
+        cursor2 = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor2.execute(f'UPDATE bookings SET status = "confirmed" WHERE booking_id = {bookingID}')
+        mysql.connection.commit()
+        return redirect(url_for('manage_bookings'))
+        
+
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM bookings ORDER BY booking_id DESC')
+    result = cursor.fetchall()
+       
+    return render_template("manage_bookings.html", data=result)
+
+
+
+@app.route("/manage_packages_add", methods=['GET', 'POST'])
+def manage_packages_add():
+
+    if request.method == 'POST' and 'PackageName' in request.form and 'Location1' in request.form and 'Location2' in request.form\
+        and 'Location3' in request.form and 'Days' in request.form and 'Amount' in request.form and 'pkID' in request.form:
+        PackageID = request.form['pkID']
+        PackageName = request.form['PackageName']
+        Location1 = request.form['Location1']
+        Location2 = request.form['Location2']
+        Location3 = request.form['Location3']
+        Days = request.form['Days']
+        Amount = request.form['Amount']
+
+        cursor2 = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor2.execute('INSERT INTO package_details VALUES (% s, % s, % s, % s, % s)',(int(PackageID)+1, PackageName, Location1, Days, Amount))
+        cursor2.execute('INSERT INTO package_details VALUES (% s, % s, % s, % s, % s)',(int(PackageID)+1, PackageName, Location2, Days, Amount))
+        cursor2.execute('INSERT INTO package_details VALUES (% s, % s, % s, % s, % s)',(int(PackageID)+1, PackageName, Location3, Days, Amount))
+        cursor2.execute('INSERT INTO packages VALUES (Null, % s, % s, % s)',(PackageName, Days, Amount)) 
+        mysql.connection.commit()
+        return redirect(url_for('manage_packages_add'))
+
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM view_packages2')
+    result = cursor.fetchall()
+
+    cursor.execute('SELECT location_id FROM locations')
+    result2 = cursor.fetchall()
+       
+    return render_template("manage_packages_add.html", data=result, len=len(result), data2=result2)
+
+
+
+@app.route("/manage_packages_delete", methods=['GET', 'POST'])
+def manage_packages_delete():
+
+    if request.method == 'POST' and 'PackageID' in request.form:
+        PackageID = request.form['PackageID']
+
+
+        cursor2 = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor2.execute(f'DELETE FROM package_details WHERE package_id = {PackageID}')
+        cursor2.execute(f'DELETE FROM packages WHERE package_id = {PackageID}') 
+        mysql.connection.commit()
+        return redirect(url_for('manage_packages_delete'))
+
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM view_packages2')
+    result = cursor.fetchall()
+
+    cursor.execute('SELECT package_id FROM packages')
+    result2 = cursor.fetchall()
+       
+    return render_template("manage_packages_delete.html", data=result, len=len(result), data2=result2)
+
+
+
+@app.route("/manage_locations_add", methods=['GET', 'POST'])
+def manage_locations_add():
+
+    if request.method == 'POST' and 'Location' in request.form and 'Country' in request.form and 'Description' in request.form:
+        Location = request.form['Location']
+        Country = request.form['Country']
+        Description = request.form['Description']
+
+        cursor2 = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor2.execute('INSERT INTO locations VALUES (NULL, % s, % s, % s)',
+                        (Location, Country, Description))
+        mysql.connection.commit()
+        return redirect(url_for('manage_locations_add'))
+
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT location_id, location_name, country, discription FROM locations')
+    result = cursor.fetchall()
+       
+    return render_template("manage_locations_add.html", data=result, len=len(result))
+
+
+@app.route("/manage_locations_update", methods=['GET', 'POST'])
+def manage_locations_update():
+
+    if request.method == 'POST' and 'LocationID' in request.form and 'Location' in request.form and 'Country' in request.form and 'Description' in request.form:
+        LocationID = request.form['LocationID']
+        Location = request.form['Location']
+        Country = request.form['Country']
+        Description = request.form['Description']
+
+        cursor2 = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor2.execute(f'UPDATE locations SET location_name = "{Location}", country = "{Country}", discription = "{Description}" WHERE location_id = {LocationID}')
+        mysql.connection.commit()
+        return redirect(url_for('manage_locations_update'))    
+
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT location_id, location_name, country, discription FROM locations')
+    result = cursor.fetchall()
+       
+    return render_template("manage_locations_update.html", data=result, len=len(result))
+
+
+@app.route("/manage_locations_delete", methods=['GET', 'POST'])
+def manage_locations_delete():
+
+    if request.method == 'POST' and 'LocationID' in request.form:
+        LocationID = request.form['LocationID']
+
+        cursor2 = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor2.execute(f'DELETE FROM locations WHERE location_id = {LocationID}')
+        mysql.connection.commit()
+        return redirect(url_for('manage_locations_delete'))       
+
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT location_id, location_name, country, discription FROM locations')
+    result = cursor.fetchall()
+       
+    return render_template("manage_locations_delete.html", data=result, len=len(result))
 
 
 
